3e9949b3eff098698c3615b2dc98bec5548017dc
linneaherbaek <lihe18ad@student.cbs.dk>
2019-09-24 08:56:05 +0200

    kodeord1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String myUsername = "linneaherbaek";
-        String myPassword = "hvadtrorduselv";
+        String myPassword = "hvadtrorduselv1";
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
